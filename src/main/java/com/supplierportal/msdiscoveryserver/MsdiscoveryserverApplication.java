6e2c0701c33d2520dfe5805501eb94f7de92276b
KAROUAOUI Ayoub <KAROUAOUI@ilemgroup.com>
2019-02-03 12:33:38 +0100

    Initial commit

---
+++ src/main/java/com/supplierportal/msdiscoveryserver/MsdiscoveryserverApplication.java
@@ -0,0 +1,14 @@
+package com.supplierportal.msdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdiscoveryserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsdiscoveryserverApplication.class, args);
+    }
+
+}
+
